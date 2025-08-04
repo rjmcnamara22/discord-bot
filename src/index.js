265d2e6e8ef4656f9dd6f3f30655dfefaa06290a
Ryan_McNamara <rjmcnamara22@gmail.com>
2025-08-04 15:22:13 -0400

    properly formatted

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-require("dotenv").config()
+require("dotenv").config();
 
-const eventHandler = require('./handlers/eventHandler')
-const generateImage = require("./generateimage")
+const eventHandler = require("./handlers/eventHandler");
+const generateImage = require("./generateimage");
 
-const { 
-    Client, 
-    GatewayIntentBits, 
-    Partials, 
-    Collection, 
-    ActivityType, 
-    PresenceUpdateStatus, 
-    Events 
-} = require("discord.js")
+const {
+  Client,
+  GatewayIntentBits,
+  Partials,
+  Collection,
+  ActivityType,
+  PresenceUpdateStatus,
+  Events,
+} = require("discord.js");
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers
-    ],
-    partials: [
-        Partials.Channel,
-        Partials.Message,
-        Partials.User,
-        Partials.GuildMember
-    ]
-})
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+  partials: [
+    Partials.Channel,
+    Partials.Message,
+    Partials.User,
+    Partials.GuildMember,
+  ],
+});
 
 eventHandler(client);
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
